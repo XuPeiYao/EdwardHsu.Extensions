d006fc44bf87068106ef99b6c348a96629eca7d8
Edward Hsu <st89382000@gmail.com>
2022-07-21 10:55:54 +0800

    feat: change di

--- src/EdwardHsu.Extensions.Configuration.Redis.StackExchange.Tests/RedisConfigurationTest.cs
+++ src/EdwardHsu.Extensions.Configuration.Redis.StackExchange.Tests/RedisConfigurationTest.cs
@@ -1,5 +1,7 @@
+using System.Net;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Moq;
 using StackExchange.Redis;
 
 namespace EdwardHsu.Extensions.Configuration.Redis.StackExchange.Tests
@@ -18,16 +20,40 @@ namespace EdwardHsu.Extensions.Configuration.Redis.StackExchange.Tests
 
             
             var configuration = builder.Build();
-            
 
 
+
+            #region Mock
+            var mockServer = new Mock<IServer>();
+            mockServer
+                .Setup(x => x.Keys(It.IsAny<int>(), It.IsAny<RedisValue>(), It.IsAny<int>(), It.IsAny<int>(),
+                    It.IsAny<int>(), It.IsAny<CommandFlags>()))
+                .Returns(new RedisKey[] { new RedisKey("TEST:ID"), new RedisKey("TEST:NAME") });
+
+
+            var mockDatabase = new Mock<IDatabase>();
+            mockDatabase
+                .Setup(x => x.StringGet(It.IsAny<RedisKey>(), CommandFlags.None))
+                .Returns("NEW");
+            mockDatabase
+                .Setup(x => x.KeyExists(It.IsAny<RedisKey>(), CommandFlags.None))
+                .Returns(true);
+
+            var mockMultiplexer = new Mock<IConnectionMultiplexer>();
+            mockMultiplexer
+                .Setup(x => x.GetEndPoints(It.IsAny<bool>()))
+                .Returns(new EndPoint[]{IPEndPoint.Parse("127.0.0.1:80")});
+            mockMultiplexer
+                .Setup(x => x.GetServer(It.IsAny<EndPoint>(), null))
+                .Returns(mockServer.Object);
+            mockMultiplexer
+                .Setup(x => x.GetDatabase(It.IsAny<int>(), null))
+                .Returns(mockDatabase.Object);
+
+            #endregion
 
             var services = new ServiceCollection();
-            services.AddSingleton(sp =>
-            {
-                ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
-                return redis;
-            });
+            services.AddSingleton(mockMultiplexer.Object);
             services.AddSingleton<StackExchangeRedisAccessProvider>();
             services.AddOptions<RedisConfigurationOptions>().Configure(x =>
             {
--- src/EdwardHsu.Extensions.Configuration.Redis.StackExchange/StackExchangeRedisAccessProvider.cs
+++ src/EdwardHsu.Extensions.Configuration.Redis.StackExchange/StackExchangeRedisAccessProvider.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Channels;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Primitives;
 using StackExchange.Redis;
@@ -8,18 +9,20 @@ namespace EdwardHsu.Extensions.Configuration.Redis.StackExchange
 {
     public class StackExchangeRedisAccessProvider:IRedisAccessProvider
     {
-        private readonly ConnectionMultiplexer _connectionMultiplexer;
+        private readonly IConnectionMultiplexer _connectionMultiplexer;
         private readonly IDatabase _database;
         private readonly IServer _server;
         private readonly RedisConfigurationOptions _options;
         private readonly ConfigurationReloadToken _reloadToken;
         private ISubscriber _subscriber;
-        public StackExchangeRedisAccessProvider(ConnectionMultiplexer connectionMultiplexer, IOptions<RedisConfigurationOptions> optional)
+        public StackExchangeRedisAccessProvider(IServiceProvider serviceProvider, IOptions<RedisConfigurationOptions> optional)
         {
+            var connectionMultiplexer = serviceProvider.GetService<ConnectionMultiplexer>() ??
+                                        serviceProvider.GetService<IConnectionMultiplexer>();
             _connectionMultiplexer = connectionMultiplexer;
             _options = optional?.Value ?? RedisConfigurationOptions.Default;
             _database = connectionMultiplexer.GetDatabase(_options.DatabaseIndex);
-            _server = connectionMultiplexer.GetServer(connectionMultiplexer.GetEndPoints().First());
+            _server = connectionMultiplexer.GetServer(connectionMultiplexer.GetEndPoints().FirstOrDefault());
             _reloadToken = new ConfigurationReloadToken();
         }
 
